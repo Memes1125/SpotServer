3d4c8ae3d4e6f91e1315b439ef040a25c2b09550
DemesMemes <73619063+Memes1125@users.noreply.github.com>
2022-12-09 21:32:38 +1000

    Getting started with the client

---
+++ SpotifyServer/Controllers/LikesTrackController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsApi;
+using SpotifyServer.db;
+
+namespace SpotifyServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LikesTrackController : ControllerBase
+    {
+        private readonly SpotyContext db;
+
+        public LikesTrackController(SpotyContext db)
+        {
+            this.db = db;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<LikesTrackApi> Get()
+        {
+            return db.LikesTracks.ToList().Select(s => (LikesTrackApi)s);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LikesTrackApi>> Get(int id)
+        {
+            var likesTrack = await db.LikesTracks.FindAsync(id);
+            if (likesTrack == null)
+                return NotFound();
+            return Ok(likesTrack);
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post([FromBody] LikesTrackApi value)
+        {
+            var newLikesTrack = (LikesTrack)value;
+            db.LikesTracks.Add(newLikesTrack);
+            await db.SaveChangesAsync();
+            return Ok(newLikesTrack.Id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] LikesTrackApi value)
+        {
+            var oldLikesTrack = await db.LikesTracks.FindAsync(id);
+            if (oldLikesTrack == null)
+                return NotFound();
+            db.Entry(oldLikesTrack).CurrentValues.SetValues(value);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var oldLikesTrack = await db.LikesTracks.FindAsync(id);
+            if (oldLikesTrack == null)
+                return NotFound();
+            db.LikesTracks.Remove(oldLikesTrack);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
+    }
+}
---
+++ SpotifyServer/Controllers/UserAlbumController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ModelsApi;
+using SpotifyServer.db;
+
+namespace SpotifyServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserAlbumController : ControllerBase
+    {
+        private readonly SpotyContext db;
+
+        public UserAlbumController(SpotyContext db)
+        {
+            this.db = db;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<UserAlbumApi> Get()
+        {
+            return db.UserAlbums.ToList().Select(s => (UserAlbumApi)s);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserAlbumApi>> Get(int id)
+        {
+            var userAlbum = await db.UserAlbums.FindAsync(id);
+            if (userAlbum == null)
+                return NotFound();
+            return Ok(userAlbum);
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult<int>> Post([FromBody] UserAlbumApi value)
+        {
+            var newUserAlbum = (UserAlbum)value;
+            db.UserAlbums.Add(newUserAlbum);
+            await db.SaveChangesAsync();
+            return Ok(newUserAlbum.Id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] UserAlbumApi value)
+        {
+            var oldUserAlbum = await db.UserAlbums.FindAsync(id);
+            if (oldUserAlbum == null)
+                return NotFound();
+            db.Entry(oldUserAlbum).CurrentValues.SetValues(value);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var oldUserAlbum = await db.UserAlbums.FindAsync(id);
+            if (oldUserAlbum == null)
+                return NotFound();
+            db.UserAlbums.Remove(oldUserAlbum);
+            await db.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
